71def133e523da1eb3417084d9681c6b4ec1b34d
Naomi <naomi.cristina@alunoescolassociais.org.br>
2024-08-09 10:03:21 -0300

    correcao do codigo

--- controller.js
+++ controller.js
@@ -1,52 +1,57 @@
 //Validar acesso em tela de login
  
-function acessar() {
-    let loginEmail = document.getElementById("loginEmail").value;
-    let loginSenha = document.getElementById("loginSenha").value;
+function acessar(){
  
-    if(!loginEmail || !loginSenha){
-        alert("Favor preencher todos os campos");
-    }else{
-      //  alert("Campos preenchidos com sucesso");
-      window.location.href = 'cadastro.html';
+   let loginEmail = document.getElementById('loginEmail').value;
+   let loginSenha = document.getElementById('loginSenha').value;
+    
+   if(!loginEmail || !loginSenha){
+     alert("Favor preencher todos os campos.");
+    
+   }else{
+      //alert("Campos preenchidos com sucesso.");
+      window.location.href= 'cadastro.html';
+    
+   }
+    
+   }
+    
+    
+   //Função que armazena, em array, nome na tela de cadastro
+    
+   var dadosLista = [];
+    
+   function salvarUser(){
+      let nomeUser = document.getElementById('nomeUser').value;
+    
+      if(nomeUser){
+       dadosLista.push(nomeUser);
+       //console.log(dadosLista);
+       crialista();
+       document.getElementById('nomeUser').value = "";
+    
+      }else{
+       alert("Favor informar o nome cadastrado");
+    
+    
+      }
+   }    
+    
+   //funçao para criar lista
+   function crialista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>nome Usuario</th><th>Açoes</th</tr>";
+   for(let i = 0; i <= (dadosLista.length-1); i++){
+      tabela += "<tr><td>"  +  dadosLista[i] + "</td><td> <button type= 'button' onclick= 'editar(this.parentNode.parentNode.rowIndex)'> Editar </button></td></tr>";
+      document.getElementById('tabela').innerHTML = tabela;
+   }
+   }
+    
+   //Função para editar nomes da lista
+    
+    function editar(i){
+      document.getElementById('nomeUser').value = dadosLista [(i - i)];
+      dadosLista.splice(dadosLista [(i - 1)], 1);
+    
+    
     }
    
-}
- 
- 
-// FUNÇAO QUE ARMAZENA EM ARRAY NOME NA TELA DE CASDASTRO
- 
-var dadosLista = [];
- 
-function salvarUser(){
-   let nomeUser = document.getElementById('nomeUser').value;
- 
-   if(nomeUser){
-    dadosLista.push(nomeUser);
-    //console.log(dadosLista);
-    crialista();
-    document.getElementById('nomeUser').value = "";
- 
-   }else{
-    alert("Favor informar o nome cadastrado");
- 
- 
-   }
-}    
- 
-//funçao para criar lista
-function crialista(){
- let tabela = document.getElementById('tabela').innerHTML = "<tr><th>nome Usuario</th><th>Açoes</th</tr>";
-for(let i = 0; i <= (dadosLista.length-1); i++){
-   tabela += "<tr><td>"  +  dadosLista[i] + "</td><td><button type='button' onclick= 'editar(this.parentNode.parentNode.rowIndex)'>editar</button></td></tr>';
-   document.getElementById('tabela').innerHTML = tabela;
-}
-}
-
-
-
-     //FUNÇÃO PARA EDITAR NOMES DA LISTA
-     function editar (i){
-        document.getElementById('nomeUser').value = dadosLista[(i - 1)];
-        dadosLista.splice(dadoslista[(i - 1)], 1);
-     }
